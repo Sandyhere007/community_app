1933af05b1986f45a6f12c85f092092d7c7e0e50
Sandy <sandeeppatidar082@gmail.com>
2023-08-11 00:11:59 +0530

    Increases time in sendcookies

--- utils/sendCookie.js
+++ utils/sendCookie.js
@@ -4,7 +4,7 @@ export const sendCookie = (user,res,message,statusCode=200) =>{
     const token = jwt.sign({_id:user._id},process.env.JWT_SECRET);
     res.status(statusCode).cookie("token",token,{
         httpOnly: true,
-        maxAge : 1000*60*15,
+        maxAge : 1000*60*60*24,
         sameSite: process.env.NODE_ENV==="Development" ? "lax" : "none",
         secure : process.env.NODE_ENV==="Development" ? false : true,
     }).json({
